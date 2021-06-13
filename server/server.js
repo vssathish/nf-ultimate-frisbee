ceaf7acc8d9546b7d2a1a8d1e77e855a0ee18e13
Sathish Sridharan <>
2021-06-13 00:14:38 -0700

    Updating mongo lab uri

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 
 const app = express();
@@ -18,15 +19,24 @@ var gm = require('./game-player-manager')
 var eb = require('./event-bus.js')
 
 // Mongoose Schema definition
-const MONGOLAB_URI='mongodb://nfUltimateUser:LetsP1ayFrisbee@ds145911.mlab.com:45911/nf-ultimate-frisbee';
+// const MONGOLAB_URI='mongodb://nfUltimateUser:LetsP1ayFrisbee@ds145911.mlab.com:45911/nf-ultimate-frisbee';
+const MLAB_URI = 'mongodb+srv://nfUltimateUser:LetsP1ayFrisbee@cluster0.zoya2.mongodb.net/nf-ultimate-frisbee?retryWrites=true&w=majority';
+
 const Schema = new mongoose.Schema({
   time       : String, 
   players    : [String]
 }),
 Game = mongoose.model('Game', Schema);
-mongoose.connect(MONGOLAB_URI, function (error) {
+mongoose.connect(MLAB_URI, function (error) {
     if (error) console.error(error);
     else console.log('mongo connected');
+});
+
+const client = new MongoClient(MLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 
