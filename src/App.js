01bde51ef8c5569d5ba873d8d0c7aa274f39c286
Sathish Sridharan <ssridharan@netflix.com>
2022-06-11 17:13:39 -0700

    Adding heroku engine spc. Fixing rel warning on anchor tags

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
               <a className="details-link" target="_blank" rel="noopener noreferrer" href={venue.map}>{venue.address}</a>
             </p>
             <p className="next-game">
-               Block the time on your calendar! <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MDQ4b2tqZHRsZGIyYmdmNGk4YTI3czhxdTdfMjAyMTA5MDFUMTkwMDAwWiBkYXJyeWwubGVlQG0&amp;tmsrc=darryl.lee%40gmail.com&amp;scp=ALL">Wed</a> | <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MDYyc3NtazB0Y3Rsa3VkbjRrNHZyOXNqYTBfMjAyMTA5MDNUMTkwMDAwWiBkYXJyeWwubGVlQG0&amp;tmsrc=darryl.lee%40gmail.com&amp;scp=ALL">Fri</a>
+               Block the time on your calendar! <a target="_blank" rel="noopener noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MDQ4b2tqZHRsZGIyYmdmNGk4YTI3czhxdTdfMjAyMTA5MDFUMTkwMDAwWiBkYXJyeWwubGVlQG0&amp;tmsrc=darryl.lee%40gmail.com&amp;scp=ALL">Wed</a> | <a target="_blank" rel="noopener noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MDYyc3NtazB0Y3Rsa3VkbjRrNHZyOXNqYTBfMjAyMTA5MDNUMTkwMDAwWiBkYXJyeWwubGVlQG0&amp;tmsrc=darryl.lee%40gmail.com&amp;scp=ALL">Fri</a>
                <br />
                <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif" />
             </p>
